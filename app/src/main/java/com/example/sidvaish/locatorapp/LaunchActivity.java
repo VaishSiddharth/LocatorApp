87fc07e51b8bc28f1e8e211f6f6b13e9708db432
VaishSiddharth <sidvaish97@gmail.com>
2018-08-25 12:09:54 +0530

    test on github toast

--- app/src/main/java/com/example/sidvaish/locatorapp/LaunchActivity.java
+++ app/src/main/java/com/example/sidvaish/locatorapp/LaunchActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class LaunchActivity extends AppCompatActivity{
 
@@ -11,6 +12,7 @@ public class LaunchActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+        Toast.makeText(this, "display", Toast.LENGTH_SHORT).show();
     }
 
     public void maps(View view)
